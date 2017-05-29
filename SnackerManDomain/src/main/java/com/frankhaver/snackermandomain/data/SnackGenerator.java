140ad692e34ed9dec85a894f0bbe4223d80f5ad6
mrhaver <frank96haver@gmail.com>
2017-05-29 22:12:45 +0200

    added snackerman domain with snack class and snack generator

---
+++ SnackerManDomain/src/main/java/com/frankhaver/snackermandomain/data/SnackGenerator.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.frankhaver.snackermandomain.data;
+
+import com.frankhaver.snackermandomain.model.Snack;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Frank Haver
+ */
+public class SnackGenerator {
+    
+    private static SnackGenerator instance;
+    
+    private SnackGenerator(){
+        
+    }
+    
+    public static SnackGenerator getInstance(){
+        if (instance == null)
+            instance = new SnackGenerator();
+        return instance;
+    }
+    
+    /**
+     * Get all different snacks without price
+     * @return all different snacks
+     */
+    public ArrayList<Snack> getAllSnacks(){
+        ArrayList<Snack> allSnacks = new ArrayList<>();
+        allSnacks.add(new Snack("Frikandel"));
+        allSnacks.add(new Snack("Viandel"));
+        allSnacks.add(new Snack("Kroket"));
+        allSnacks.add(new Snack("Nasipuk"));
+        allSnacks.add(new Snack("Bamischijf"));
+        allSnacks.add(new Snack("Patatje Ruzie"));
+        allSnacks.add(new Snack("Berenbal"));
+        allSnacks.add(new Snack("Mexicaantje"));
+        allSnacks.add(new Snack("Hotwings"));
+        allSnacks.add(new Snack("Kipnuggets"));
+        allSnacks.add(new Snack("Halve haan"));
+        allSnacks.add(new Snack("Kipcorn"));
+        allSnacks.add(new Snack("Maiskolf"));
+        return allSnacks;
+    }
+    
+    /**
+     * Get all snacks and give all snacks a random price
+     * @return all snacks with price
+     */
+    public ArrayList<Snack> randomPriceAllSnacks(){
+        return null;
+    }
+}
---
+++ SnackerManDomain/src/main/java/com/frankhaver/snackermandomain/model/Snack.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.frankhaver.snackermandomain.model;
+
+/**
+ *
+ * @author Frank Haver
+ */
+public class Snack {
+    
+    private String name;
+    private double price;
+    
+    public Snack(){
+        
+    }
+    
+    public Snack(String name){
+        this.name = name;
+    }
+    
+    public Snack(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    
+}
